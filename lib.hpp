252a75f2098c5d54b5fca57c5596d7d671b7055d
MargaritaSeil <90559062+MargaritaSeil@users.noreply.github.com>
2021-12-03 20:51:01 +0200

    Add files via upload

---
+++ lib.hpp
@@ -0,0 +1,16 @@
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setw;
+using std::string;
+using std::setprecision;
+using std::left;
+using std::vector; 
+using std::accumulate;
+using std::domain_error;
+using std::fstream;
+using std::ifstream;
+using std::ofstream;
+using std::getline;
+using std::istringstream;
+using std::to_string;
